436b89bf2f7f0ae9b428a38749f3ec2011403c36
Chethan Pandarinath <cpandar@gmail.com>
2018-11-17 15:06:03 -0500

    added script to generate pdf

---
+++ make_pdf.sh
@@ -0,0 +1,7 @@
+# requires homebrew, node, npm
+# then requires the markdown pdf package:
+#    https://github.com/alanshaw/markdown-pdf
+# installed via:
+#    npm install -g markdown-pdf
+
+markdown-pdf -o labmanual.pdf README.md 
