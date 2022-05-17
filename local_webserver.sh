49ede6373f42e60fe8a37e86dd46e7789c77dd75
Chethan Pandarinath <cpandar@dynamic.bme.emory.edu>
2022-05-17 13:08:17 -0400

    adding mkdocs-cinder directly to repo

--- local_webserver.sh
+++ local_webserver.sh
@@ -2,8 +2,9 @@
 # mkdocs must be installed already
 # on my personal laptop (OS X), which has homebrew installed, this was done via:
 #     brew install mkdocs
-# also, to get the cinder theme, I then did
-#     brew install mkdocs-cinder
+## also, to get the cinder theme, I then did
+##     brew install mkdocs-cinder
+#CP note 2022-05-17 - mkdocs-cinder no longer installable via brew, just added it directly to the repo
 
 # make sure you're in this directory.
 
